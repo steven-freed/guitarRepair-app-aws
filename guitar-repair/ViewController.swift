8adcb65e799729b86173707f1470201b44365e8e
steven freed <stevenfreed@stevens-air-2.home>
2018-06-05 11:35:40 -0400

    Initial Commit

---
+++ guitar-repair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guitar-repair
+//
+//  Created by steven freed on 6/5/18.
+//  Copyright © 2018 steven freed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
